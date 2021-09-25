9f3cffd15ecdce537c0beafdb74b4db315d99959
wmc425454438 <superwong0827@gmail.com>
2021-09-25 09:44:45 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <a-layout id="components-layout">
+    <a-layout-header>Header</a-layout-header>
+    <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout-content>Content</a-layout-content>
+    </a-layout>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
+</template>
+
+<style scoped>
+#components-layout {
+  height: 100%;
+}
+</style>
