40663c36bee931aa40669588feded8ab99310ce9
wmc425454438 <superwong0827@gmail.com>
2021-09-25 23:51:26 +0800

    feat: 注册 登陆 清空登陆

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import Home from './components/Home.vue'
+import Login from './components/Login.vue'
 </script>
 
 <template>
 <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
 <!-- <HelloWorld msg="Hello Vue 3 + Vite"></HelloWorld> -->
 <Home></Home>
+<Login></Login>
 </template>
 
 <style>
---
+++ src/components/NotFoundPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    404 Page Not Found
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    font-size: 40px;
+}
+</style>
